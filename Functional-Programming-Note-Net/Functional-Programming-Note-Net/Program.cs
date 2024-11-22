6040b469747bacad04ea1d20e7f88bb92202c0f3
Raymond Lei(雷菩宇) <ray810815@gmail.com>
2024-11-22 09:30:32 -0500

    FrozenDictionary in C# Note

---
+++ Functional-Programming-Note-Net/Functional-Programming-Note-Net/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Frozen;
+using System.Linq;
+using Test;
+
+
+FrozenDictionary<int, StudentName>? frozenDictionary = Enumerable.Range(1, 10)
+    .Select(i => new StudentName { FirstName = $"First-{i}", LastName = $"Last-{i}", ID = i })
+    .ToFrozenDictionary(x => x.ID, x => x);
+
+FrozenSet<int> frozenSet = frozenDictionary.Keys.ToFrozenSet();
+namespace Test
+{
+
+    public record StudentName(string FirstName, string LastName, int ID);
+}
+
+//copy to a frozenset 
