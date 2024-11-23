948004c7ca095ea5d46bb4943e0ca4dee5d276c5
Raymond Lei(雷菩宇) <ray810815@gmail.com>
2024-11-22 21:29:27 -0500

    debug

--- Functional-Programming-Note-Net/Functional-Programming-Note-Net/Program.cs
+++ Functional-Programming-Note-Net/Functional-Programming-Note-Net/Program.cs
@@ -1,17 +1,20 @@
-﻿using System.Collections.Frozen;
+﻿using Functional_Programming_Note_Net;
+using System.Collections.Frozen;
 using System.Linq;
-using Test;
 
 
-FrozenDictionary<int, StudentName>? frozenDictionary = Enumerable.Range(1, 10)
-    .Select(i => new StudentName { FirstName = $"First-{i}", LastName = $"Last-{i}", ID = i })
+FrozenDictionary<int, StudentName> frozenDictionary = Enumerable.Range(1, 10)
+    .Select(static i => new StudentName($"First-{i}", LastName: $"Last-{i}", ID: i))
     .ToFrozenDictionary(x => x.ID, x => x);
 
-FrozenSet<int> frozenSet = frozenDictionary.Keys.ToFrozenSet();
-namespace Test
-{
-
-    public record StudentName(string FirstName, string LastName, int ID);
-}
 
 //copy to a frozenset 
+
+//copy keys to a frozenset 
+FrozenSet<int> frozenSet = frozenDictionary.Keys.ToFrozenSet();
+
+//
+Lazy<FrozenDictionary<int, StudentName>> lazyFrozenSet = new(static () => 
+    Enumerable.Range(1, 10)
+        .Select(static i => new StudentName($"First-{i}", LastName: $"Last-{i}", ID: i))
+        .ToFrozenDictionary(x => x.ID, x => x));
---
+++ Functional-Programming-Note-Net/Functional-Programming-Note-Net/StudentName.cs
@@ -0,0 +1,5 @@
+﻿namespace Functional_Programming_Note_Net;
+
+public record StudentName(string FirstName, string LastName, int ID);
+
+//copy to a frozenset 
